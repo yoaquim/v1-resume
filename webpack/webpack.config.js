44897c80f9f420eb40b38d4707f7e92d1170ef2b
Yoaquim Cintrón <yoaquim@outlook.com>
2017-05-02 21:04:10 -0400

    Building bundle to webpack dir. No building to public dir

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         './styles/app.less'
     ],
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, '../public'),
         filename: 'app.js'
     },
     devServer: {
